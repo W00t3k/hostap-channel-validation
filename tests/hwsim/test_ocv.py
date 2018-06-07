a1e0e28855abcec023c1e45e0d6ce06391d176e5
Mathy <vanhoefm@users.noreply.github.com>
2018-06-06 22:15:23 -0400

    Prototype STA tests

--- tests/hwsim/test_ocv.py
+++ tests/hwsim/test_ocv.py
@@ -284,4 +284,129 @@ def test_wpa2_ocv_ap_unexpected2(dev, apdev):
     conn.confirm_valid_oci(81, 1, 0)
 
 
+class STAConnection:
+    def __init__(self):
+        self.hapd = None
+        self.dev  = None
+        self.bssid = None
+        self.addr = None
+        self.rsne = None
+        self.gtkie = None
+        self.kck  = None
+        self.kek  = None
+        self.msg  = None
+        self.anonce = None
+        self.snonce = None
+
+    def __init__(self, apdev, dev, params, sta_params=None):
+        self.dev = dev
+        self.bssid = apdev['bssid']
+
+        freq = params.pop("freq")
+        if sta_params is None:
+            sta_params = dict()
+        if not "ocv" in sta_params:
+            sta_params["ocv"] = "1"
+        if not "ieee80211w" in sta_params:
+            sta_params["ieee80211w"] = "1"
+
+        ssid = "test-wpa2-ocv"
+        passphrase = "qwertyuiop"
+        psk = "c2c6c255af836bed1b3f2f1ded98e052f5ad618bb554e2836757b55854a0eab7"
+        params.update(hostapd.wpa2_params(ssid=ssid, passphrase=passphrase))
+        params['wpa_pairwise_update_count'] = "10"
+
+        self.hapd = hostapd.add_ap(apdev, params)
+        self.hapd.request("SET ext_eapol_frame_io 1")
+        self.dev.request("SET ext_eapol_frame_io 1")
+        pmk = binascii.unhexlify("c2c6c255af836bed1b3f2f1ded98e052f5ad618bb554e2836757b55854a0eab7")
+
+        self.gtkie = binascii.unhexlify("dd16000fac010100dc11188831bf4aa4a8678d2b41498618")
+        if sta_params["ocv"] != "0":
+            # FIXME: Can we avoid hostapd from adding the last element????
+            self.rsne = binascii.unhexlify("30140100000fac040100000fac040100000fac028c40")
+            #self.rsne = binascii.unhexlify("301a0100000fac040100000fac040100000fac0280400000000fac06")
+        else:
+            self.rsne = binascii.unhexlify("30140100000fac040100000fac040100000fac028c00")
+            #self.rsne = binascii.unhexlify("301a0100000fac040100000fac040100000fac0280000000000fac06")
+
+        self.dev.connect(ssid, raw_psk=psk, scan_freq=freq, wait_connect=False, **sta_params)
+        self.addr = dev.p2p_interface_addr()
+
+        # Forward msg 1/4 from AP to STA
+        self.msg = recv_eapol(self.hapd)
+        self.anonce = self.msg['rsn_key_nonce']
+        send_eapol(self.dev, self.bssid, build_eapol(self.msg))
+
+        # Capture msg 2/4 from the STA so we can derive the session keys
+        self.msg = recv_eapol(dev)
+        self.snonce = self.msg['rsn_key_nonce']
+        (ptk, self.kck, self.kek) = pmk_to_ptk(pmk, self.addr, self.bssid, self.snonce,self.anonce)
+
+        self.counter = struct.unpack('>Q', self.msg['rsn_replay_counter'])[0] + 1
+
+
+    def test_bad_oci(self, logmsg, op_class, channel, seg1_idx, errmsg):
+        # FIXME: Only print errmsg?
+        logger.info("Bad OCI element: " + logmsg)
+        if op_class is None:
+            ocikde = ""
+        else:
+            ocikde = make_ocikde(op_class, channel, seg1_idx)
+
+        plain = self.rsne + self.gtkie + ocikde
+        wrapped = aes_wrap(self.kek, pad_key_data(plain))
+        msg = build_eapol_key_3_4(self.anonce, self.kck, wrapped, replay_counter=self.counter)
+
+        self.dev.dump_monitor()
+        send_eapol(self.dev, self.bssid, build_eapol(msg))
+        self.counter += 1
+
+        ev = self.dev.wait_event([errmsg], timeout=5)
+        if ev is None:
+            raise Exception("Bad OCI not reported")
+
+
+    def confirm_valid_oci(self, op_class, channel, seg1_idx):
+        logger.debug("Valid OCI element to complete handshake")
+        ocikde = make_ocikde(op_class, channel, seg1_idx)
+
+        plain = self.rsne + self.gtkie + ocikde
+        wrapped = aes_wrap(self.kek, pad_key_data(plain))
+        msg = build_eapol_key_3_4(self.anonce, self.kck, wrapped, replay_counter=self.counter)
+
+        self.dev.dump_monitor()
+        send_eapol(self.dev, self.bssid, build_eapol(msg))
+        self.counter += 1
+
+        self.dev.wait_connected(timeout=1)
+
+@remote_compatible
+def test_wpa2_ocv_mismatch_client(dev, apdev):
+    params = { "channel": "1", "ieee80211w": "1", "ocv": "1", "freq": "2412"}
+    conn = STAConnection(apdev[0], dev[0], params)
+    conn.test_bad_oci("element missing", None, 0, 0, "AP did not include OCI")
+    conn.test_bad_oci("wrong channel number", 81, 6, 0, "Primary channel mismatch")
+    conn.test_bad_oci("invalid channel number", 81, 0, 0, "Unable to interpret the OCI")
+    conn.test_bad_oci("wrong operating class", 80, 0, 0, "Unable to interpret the OCI")
+    conn.test_bad_oci("invalid operating class", 0, 0, 0, "Unable to interpret the OCI")
+    conn.confirm_valid_oci(81, 1, 0)
+
+@remote_compatible
+def test_wpa2_ocv_vht160_mismatch_client(dev, apdev):
+    params = { "hw_mode": "a", "channel": "100", "country_code": "ZA",
+               "ht_capab": "[HT40+]", "ieee80211w": "1", "ieee80211n": "1",
+               "ieee80211ac": "1", "vht_oper_chwidth": "2", "ocv": "1",
+               "vht_oper_centr_freq_seg0_idx": "114", "freq": "5500",
+               "ieee80211d": "1", "ieee80211h": "1" }
+    sta_params = { "disable_vht": "1" }
+    conn = STAConnection(apdev[0], dev[0], params, sta_params)
+    conn.test_bad_oci("smaller bandwidth (20 Mhz) than negotiated", 121, 100, 0, "Channel bandwidth mismatch")
+    conn.test_bad_oci("wrong frequency, bandwith, and secondary channel", 123, 104, 0, "Primary channel mismatch")
+    conn.test_bad_oci("wrong upper/lower behaviour", 129, 104, 0, "Primary channel mismatch")
+    #conn.test_bad_oci("smaller bandwidth (40 Mhz) than negotiated", 122, 100, 0, "Channel bandwidth mismatch")
+
+    conn.confirm_valid_oci(122, 100, 0)
+    #conn.confirm_valid_oci(129, 100, 0)
+
 # ex:set ts=4 et:
