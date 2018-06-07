68901faadb6ca4553a44d4d183796d07f3d1adb8
Mathy <vanhoefm@users.noreply.github.com>
2018-06-06 22:17:26 -0400

    STA: parse OCI KDE IE

--- src/rsn_supp/wpa_ie.h
+++ src/rsn_supp/wpa_ie.h
@@ -53,6 +53,8 @@ struct wpa_eapol_ie_parse {
 	const u8 *ip_addr_req;
 	const u8 *ip_addr_alloc;
 #endif /* CONFIG_P2P */
+	const u8 *oci;
+	size_t oci_len;
 };
 
 int wpa_supplicant_parse_ies(const u8 *buf, size_t len,
